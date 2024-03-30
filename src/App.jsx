d50cc87b49e9e52ca392f0636e5342d4a87f7398
ojumbo.do <ojumbo98@gmail.com>
2024-03-30 13:57:59 +0300

    Use navigate component to redict you in a page and replace to take you back in history

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Form from "./components/Form";
 
@@ -49,10 +49,7 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
 
           <Route
             path="cities"
